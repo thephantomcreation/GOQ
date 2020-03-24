9bc038880bccc70b05bd8580365d50b4942901fd
prabinshrestha737 <prabinshrestha737@gmail.com>
2020-03-24 11:50:08 +0545

    db design

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from core.models import * 
 
 # Register your models here.
+
+
+admin.site.register(Faculty)
+admin.site.register(YearOrSemester)
+admin.site.register(Questions)
+
--- core/models.py
+++ core/models.py
@@ -1,3 +1,53 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+sem_choices = [
+        ('First', 'I'),
+        ('Second', 'II'),
+        ('Third', 'III'),
+        ('Fourth', 'IV'),
+        ('Fifth', 'V'),
+        ('Sixth','VI'),
+        ('Seventh','VII'),
+        ('Eighth','VIII')]
+
+year_choices = [
+        ('First', 'I'),
+        ('Second', 'II'),
+        ('Third', 'III'),
+        ('Fourth', 'IV'),
+        ]
+
+level_choices = [
+    ('SEE','SEE'),
+    ('Plus2','Plus2'),
+    ('Bachelor','Bachelor'),
+    ('Master','Master'),
+]
+class Faculty(models.Model):
+    level = models.CharField(max_length=15,choices=level_choices)
+    faculty = models.CharField(max_length=30, blank=True)
+    def __str__(self):
+       return self.faculty
+class YearOrSemester(models.Model):
+    faculty = models.ForeignKey(Faculty, on_delete=models.CASCADE)
+    year = models.CharField(max_length=15, choices=year_choices, blank=True)
+    sem = models.CharField(max_length=30, choices=sem_choices, blank=True, null= True)
+
+    def __str__(self):
+        return '%s %s' % (self.year, self.sem)
+
+
+
+
+
+class Questions(models.Model):
+    questions = models.ForeignKey(YearOrSemester, on_delete=models.CASCADE)
+    sub_name = models.CharField(max_length=125)
+    date_posted = models.DateTimeField(default=timezone.now)
+    file = models.FileField()
+
+    def __str__(self):
+        return self.sub_name
