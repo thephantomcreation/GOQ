b1d4bbe941d84a1a8632f4a4e1b40d9b26869657
beingbiplov <beingbiplov@gmail.com>
2020-03-25 10:23:04 +0545

    details pages design

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import index
+from .views import index, semester, questions
 
 urlpatterns = [
     path('', index, name='index' ),
+    path('sem', semester),
+    path('ques', questions)
 ]
--- core/views.py
+++ core/views.py
@@ -2,3 +2,9 @@ from django.shortcuts import render
 
 def index(request):
 	return render(request, 'core/index.html')
+
+def semester(request):
+	return render(request, 'core/semesters.html')
+
+def questions(request):
+	return render(request,'core/questions.html')
