ebd1a16aa529dbb34f3e9f033c8745165afcdb00
beingbiplov <beingbiplov@gmail.com>
2020-03-30 22:28:39 +0545

    contact us page design

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import index, semester, questions, search_results
+from .views import index, semester, questions, search_results, contact_us
 
 urlpatterns = [
     path('', index, name='index' ),
     path('sem', semester),
     path('ques', questions),
-    path('search-results', search_results, name='search_results')
+    path('search-results', search_results, name='search_results'),
+    path('contact-us', contact_us, name='contact_us')
 ]
